53f8c1332a19bc73cad974692a20099dc2f626d7
giang.dongoc <giang.dongoc@vti.com.vn>
2024-06-06 09:23:35 +0700

    🦺 Enable middleware morgan

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const { env } = require('./config');
 
 const app = express();
 
+if (env.nodeEnv === 'development') {
+  app.use(require('morgan')('dev'));
+}
+
 app.get('/', (req, res) => {
   res.send('Server is running 🚀');
 });
